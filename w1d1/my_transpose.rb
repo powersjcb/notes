f2cd5de4d984113658650279f2e59d8c89e2e59e
gconen <gregconen@gmail.com>
2015-04-20 21:21:17 -0700

    Update my_transpose.rb

--- w1d1/my_transpose.rb
+++ w1d1/my_transpose.rb
@@ -4,6 +4,7 @@ class Array
     (0..self.length - 1).each do |col_idx|
       sub_array = []
       (0..self.length - 1).each do |row_idx|
+        # sub array is row of solution
         sub_array << self[row_idx][col_idx]
       end
       transposed << sub_array

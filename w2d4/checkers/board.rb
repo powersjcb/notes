0a418d9151de106b48234421962fab511811999a
Jacob Powers <powersjcb@gmail.com>
2015-04-29 10:04:22 -0700

    More work on checkers

--- w2d4/checkers/board.rb
+++ w2d4/checkers/board.rb
@@ -18,6 +18,20 @@ class Board
     @grid[x][y] = val
   end
 
+  def occupied?(pos)
+    x, y = pos
+    !@grid[x][y].nil?
+  end
+
+  def on_board?(pos)
+    x, y = pos
+    x.between?(0,7) && y.between?(0,7)
+  end
+
+  def place_new_piece(row, col, color)
+    options = {location: [row, col], color: color}
+    @grid[row][col] = Piece.new(options)
+  end
 
   def setup_board
     rows = [0, 1, 2, 5, 6, 7]
@@ -29,29 +43,20 @@ class Board
       end
     end
   end
-
-
-
-
-  def place_new_piece(row, col, color)
-    options = {location: [row, col], color: color}
-    @grid[row][col] = Piece.new(options)
-  end
-
-
 end
 
 
 #tests
 b = Board.new
-b.grid.each do |row|
-  display_line = []
-  row.each do |space|
-    if space.nil?
-      display_line << "_"
-    else
-      display_line << space.symbol
-    end
-  end
-  puts display_line.join("|")
-end
+p b.grid[1][0].moves
+# b.grid.each do |row|
+#   display_line = []
+#   row.each do |space|
+#     if space.nil?
+#       display_line << "_"
+#     else
+#       display_line << space.symbol
+#     end
+#   end
+#   puts display_line.join("|")
+# end
--- w2d4/checkers/piece.rb
+++ w2d4/checkers/piece.rb
@@ -3,14 +3,25 @@ class Piece
     @location = options[:location]
     @color    = options[:color]
     @king     = false
+    @board    = options[:board]
+  end
+
+  def moves
+    candidates = [[1,1], [1,-1]]
+    candidates << [[-1,1], [-1,-1]] if @king == true
+    # reject if hits unit, reject if out of bounds
+    valid_moves = [ ]
+    candidates.each do |pos|
+      unless !@board.on_board?(pos) || @board.occupied?(pos)
+        valid_moves << pos
+      end
+    end
+
+    valid_moves
   end
 
   def symbol
     @color == :white ? "○" : "●" if @king == false
     @color == :white ? "◇" : "◆" if @king == true
-  end
-
-  def moves
-
   end
 end
